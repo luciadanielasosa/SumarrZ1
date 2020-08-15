bb3fe79a0e88b6c95f0fe9558f2a40e40e8197a8
luciadanielasosa <a2019118793@estudiantes.upsa.edu.bo>
2020-08-15 10:21:21 +0200

    guardar mi primer modelo

---
+++ sumar-restar/sumar/Controllers/peoplecontroller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using sumar.Models;
+
+namespace sumar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class peoplecontroller : ControllerBase
+    {[HttpGet]
+    public Person listar () {
+            Person person = new Person()
+            {
+                PersonId = 1,
+                Name = "Lucia Sosa"
+            };
+            return person;
+
+        }
+    }
+}
